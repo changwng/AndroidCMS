4342d2bc3810738c3c74806a196ab4a1c158de2c
Satan <1603411701@qq.com>
2016-03-22 22:27:22 +0800

    手动创建按钮以及监听事件

--- app/src/main/java/com/cms/satan/androidcms/MainActivity.java
+++ app/src/main/java/com/cms/satan/androidcms/MainActivity.java
@@ -2,6 +2,13 @@ package com.cms.satan.androidcms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //创建一个线性布局管理器
+        LinearLayout layout = new LinearLayout(this);
+        //设置该Activity显示layou
+        super.setContentView(layout);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        //创建一个TextView
+        final TextView show = new TextView(this);
+        //创建一个按钮
+        Button bn = new Button(this);
+        bn.setText(R.string.ok);
+        bn.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        //向layout容器中添加textview
+        layout.addView(show);
+        //向layout容器中添加按钮
+        layout.addView(bn);
+        //为按钮绑定一个事件监听器
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show.setText("触发了一次按钮的Onclick事件 ," + new Date());
+            }
+        });
 
-//        新添加一个按钮
+
+
     }
 }
